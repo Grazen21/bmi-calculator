5636c5cf073770b3af507cf7b4a1e4d1c2ba71fe
LEE SEAN WEI <sean.wei21@gmail.com>
2020-01-22 16:53:51 +0800

    Added button and changed console log to alert

--- bmi-calculator.js
+++ bmi-calculator.js
@@ -1,21 +1,23 @@
-let YourWeight=prompt("What is your weight(KG)?");
-let YourHeight=prompt("What is your height(Meters)?");
-let bmi= Math.round(YourWeight/Math.pow(YourHeight, 2)* 100) / 100;
-console.log(bmi);
-if(0 < bmi  && bmi <= 40) {
-    if (bmi <= 18){
-        console.log(`Your bmi is ${bmi}. You are underweight, please more!`)
+start = () => {
+    let YourWeight=prompt("What is your weight(KG)?");
+    let YourHeight=prompt("What is your height(Meters)?");
+    let bmi= Math.round(YourWeight/Math.pow(YourHeight, 2)* 100) / 100;
+    console.log(bmi);
+    if(0 < bmi  && bmi <= 40) {
+        if (bmi <= 18){
+            alert(`Your bmi is ${bmi}. You are underweight, please more!`)
+        }
+        if(18 < bmi && bmi <= 25){
+            alert(`Your bmi is ${bmi}. You are in the healthy weight range.`)
+        }
+        if(25 < bmi && bmi <= 30){
+            alert(`Your bmi is ${bmi}. You are overweight, please work out.`)
+        }
+        if(30<bmi && bmi<=40) {
+            alert(`Your bmi is ${bmi}. You are obese, please work out and exercise immediately!`)
+        }
     }
-    if(18 < bmi && bmi <= 25){
-        console.log(`Your bmi is ${bmi}. You are in the healthy weight range.`)
+    else {
+        alert(`Please enter your actual weight.`)
     }
-    if(25 < bmi && bmi <= 30){
-        console.log(`Your bmi is ${bmi}. You are overweight, please work out.`)
-    }
-    if(30<bmi && bmi<=40) {
-        console.log(`Your bmi is ${bmi}. You are obese, please work out and exercise immediately!`)
-    }
-}
-else {
-    console.log(`Please enter your actual weight.`)
 }
